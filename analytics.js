9e3b3f4b44050703162ff1c1f3fe30073ab4200e
Loi Tran <loi.tranquang@gmail.com>
2026-06-03 22:22:58 +0700

    Revert "Merge pull request #1 from loitq07/vercel/install-vercel-web-analytics-0pksb8"
    This reverts commit 3dbcf1f818424ab897d7c57cb8ccfdd217a5e83d, reversing
    changes made to b2644fd91afc8e34cb71b046731ef1fb1c243ae5.

--- analytics.js
+++
@@ -1,7 +0,0 @@
-/**
- * Vercel Web Analytics Initialization
- * This script initializes Vercel Web Analytics for the Key Launcher site.
- */
-import { inject } from '@vercel/analytics';
-
-inject();
